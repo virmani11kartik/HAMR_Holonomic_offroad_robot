594c0c07053dc606105832187675545c4c9142ee
virmani11kartik <kartik8virmani@gmail.com>
2025-10-24 12:44:48 -0400

    10/24 updates

--- tof_scan.cpp
+++ tof_scan.cpp
@@ -1,23 +1,18 @@
 #include <Wire.h>
 #include <Adafruit_VL53L0X.h>
 
-// I2C pins
+
 #define SDA_PIN 36
 #define SCL_PIN 35
-
-// XSHUT pins for 4 sensors
 #define XSHUT_1 37
 #define XSHUT_2 38
 #define XSHUT_3 39
 #define XSHUT_4 40
-
-// New I2C addresses for each sensor
 #define ADDR_1 0x30
 #define ADDR_2 0x31
 #define ADDR_3 0x32
 #define ADDR_4 0x33
 
-// Sensor objects
 Adafruit_VL53L0X tof1;
 Adafruit_VL53L0X tof2;
 Adafruit_VL53L0X tof3;
@@ -37,12 +32,12 @@ void readSensor(Adafruit_VL53L0X &lox, const char *label) {
 }
 
 bool initSensor(Adafruit_VL53L0X &lox, int xshutPin, int newAddr) {
-  digitalWrite(xshutPin, HIGH);   // power on
+  digitalWrite(xshutPin, HIGH);  
   delay(50);
   if (!lox.begin(0x29, false, &Wire)) {
     return false;
   }
-  lox.setAddress(newAddr);       // assign unique address
+  lox.setAddress(newAddr);      
   delay(10);
   return true;
 }
@@ -50,24 +45,16 @@ bool initSensor(Adafruit_VL53L0X &lox, int xshutPin, int newAddr) {
 void setup() {
   Serial.begin(115200);
   while (!Serial) delay(1);
-
-  // Init I2C
   Wire.begin(SDA_PIN, SCL_PIN);
-
-  // Setup XSHUT pins
   pinMode(XSHUT_1, OUTPUT);
   pinMode(XSHUT_2, OUTPUT);
   pinMode(XSHUT_3, OUTPUT);
   pinMode(XSHUT_4, OUTPUT);
-
-  // Turn all off
   digitalWrite(XSHUT_1, LOW);
   digitalWrite(XSHUT_2, LOW);
   digitalWrite(XSHUT_3, LOW);
   digitalWrite(XSHUT_4, LOW);
   delay(10);
-
-  // Bring them up one by one and change address
   if (!initSensor(tof1, XSHUT_1, ADDR_1)) Serial.println("Sensor 1 failed");
   if (!initSensor(tof2, XSHUT_2, ADDR_2)) Serial.println("Sensor 2 failed");
   if (!initSensor(tof3, XSHUT_3, ADDR_3)) Serial.println("Sensor 3 failed");
@@ -75,7 +62,6 @@ void setup() {
 
   Serial.println("All sensors initialized.");
 }
-
 
 void loop() {
   readSensor(tof1, "TOF1");
